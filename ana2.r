3a3debcd274dd8848451253c020c4e15f09964cf
sirenfrappe <sirenfrappe@gmail.com>
2019-06-14 19:31:30 +0800

    6.14
    上一次基础上加了无序与否差异性分析

--- ana2.r
+++ ana2.r
@@ -6,7 +6,7 @@ filePath <- sapply(fileName,function(x){
 data <- lapply(filePath,function(x){
   read.table(x,header = T,sep = "\t",quote = NULL,stringsAsFactors = F)
 })
-data.diease <- lapply(data, function(x){
+data.disease <- lapply(data, function(x){
   subset(x,x$RelatedDisease!=0)
 })
 
@@ -34,14 +34,14 @@ data.rate.normaldisease <- data.frame(matrix(NA,143,2))
 colnames(data.rate.normaldisease) <- c("normal","disease")
 #分母：PTM个数*蛋白长度
 for (i in 1:143) {
-  data.rate.normaldisease[i,2] <- dim(data.diease[[i]])[1]/(dim(data[[i]])[1]*
+  data.rate.normaldisease[i,2] <- dim(data.disease[[i]])[1]/(dim(data[[i]])[1]*
                                                               dim(
                                                                 subset(
                                                                   data[[i]],
                                                                   data[[i]]$Modification!=0)
                                                               )[1]
   )
-  data.rate.normaldisease[i,1] <- (dim(subset(data[[i]],data[[i]]$Modification!=0))[1]-dim(data.diease[[i]])[1])/(dim(data[[i]])[1]*
+  data.rate.normaldisease[i,1] <- (dim(subset(data[[i]],data[[i]]$Modification!=0))[1]-dim(data.disease[[i]])[1])/(dim(data[[i]])[1]*
                                                                                                                     dim(
                                                                                                                       subset(
                                                                                                                         data[[i]],
@@ -62,3 +62,75 @@ bartlett.test(c(data.rate.normaldisease[,1],data.rate.normaldisease[,2])~factor(
 #远小于0.05，采用非参数检验
 wilcox.test(data.rate.normaldisease[,1],data.rate.normaldisease[,2],paired=TRUE)
 #p-value < 2.2e-16
+
+data.PTM <- lapply(data, function(x){
+  subset(x,x$Modification!=0)
+})
+#疾病、无序/结构，打分>0.501判断
+data.disease.stru <- lapply(data.disease, function(x){
+  subset(x,x$mobidb.disorder.predictors.mobidb.lite.score<0.501)
+})
+data.disease.diso <- lapply(data.disease, function(x){
+  subset(x,x$mobidb.disorder.predictors.mobidb.lite.score>0.501)
+})
+#疾病无关、无序/结构，打分>0.501判断
+data.normal <- lapply(data, function(x){
+  subset(x,x$RelatedDisease==0 & x$Modification!=0 )
+})
+data.normal.stru <- lapply(data.normal, function(x){
+  subset(x,x$mobidb.disorder.predictors.mobidb.lite.score<0.501)
+})
+data.normal.diso <- lapply(data.normal, function(x){
+  subset(x,x$mobidb.disorder.predictors.mobidb.lite.score>0.501)
+})
+#疾病不相关PTM位点无序与否差异性检测
+data.rate.NPTM <- data.frame(matrix(NA,143,2))
+colnames(data.rate.NPTM) <- c("stru","diso")
+#疾病无关
+#分母：PTM个数*蛋白长度
+for (i in 1:143) {
+  data.rate.NPTM[i,1] <- dim(data.normal.stru[[i]])[1]/(dim(data[[i]])[1]*
+                                                          dim(
+                                                            subset(
+                                                              data[[i]],
+                                                              data[[i]]$Modification!=0)
+                                                          )[1])
+  data.rate.NPTM[i,2] <- dim(data.normal.diso[[i]])[1]/(dim(data[[i]])[1]*
+                                                          dim(
+                                                            subset(
+                                                              data[[i]],
+                                                              data[[i]]$Modification!=0)
+                                                          )[1])
+}
+shapiro.test(data.rate.NPTM[,1])
+shapiro.test(data.rate.NPTM[,2])
+#p<0.05
+bartlett.test(c(data.rate.NPTM[,1],data.rate.NPTM[,2])~factor(c(rep(1,143),rep(2,143))))
+#p-value = 1.043e-08
+wilcox.test(data.rate.NPTM[,1],data.rate.NPTM[,2],paired=TRUE)
+#p-value = 6.57e-10，差异性显著
+
+#疾病相关
+data.rate.DPTM <- data.frame(matrix(NA,143,2))
+colnames(data.rate.DPTM) <- c("stru","diso")
+for (i in 1:143) {
+  data.rate.DPTM[i,1] <- dim(data.disease.stru[[i]])[1]/(dim(data[[i]])[1]*
+                                                          dim(
+                                                            subset(
+                                                              data[[i]],
+                                                              data[[i]]$Modification!=0)
+                                                          )[1])
+  data.rate.DPTM[i,2] <- dim(data.disease.diso[[i]])[1]/(dim(data[[i]])[1]*
+                                                          dim(
+                                                            subset(
+                                                              data[[i]],
+                                                              data[[i]]$Modification!=0)
+                                                          )[1])
+}
+shapiro.test(data.rate.DPTM[,1])
+shapiro.test(data.rate.DPTM[,2])
+#p<0.05
+bartlett.test(c(data.rate.DPTM[,1],data.rate.DPTM[,2])~factor(c(rep(1,143),rep(2,143))))
+# p-value < 2.2e-16
+wilcox.test(data.rate.DPTM[,1],data.rate.DPTM[,2],paired=TRUE)
+#p-value = 5.421e-07，差异性显著
