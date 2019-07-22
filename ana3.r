4ab77e684a1eb9fe50b8977e893d0d517f06ae7f
sirenfrappe <sirenfrappe@gmail.com>
2019-07-22 10:11:24 +0800

    7.22

--- ana3.r
+++ ana3.r
@@ -19,11 +19,11 @@ leng <- 365
 sequ<-  data[[1]][,2]
 length(sequ)
 
-dfP <- data.frame(matrix(1,leng+1,leng+1))
+dfP <- matrix(1,leng+1,leng+1)
 dfP[1,2:(leng+1)] <- sequ
 dfP[2:(leng+1),1] <- sequ
 
-df01 <- data.frame(matrix(1,leng,leng))
+df01 <- matrix(1,leng,leng)
 
 
 for (m in 1:dim(rr)[1]) {
@@ -43,5 +43,14 @@ library(pheatmap)
 
 pheatmap(df01,cluster_row = FALSE, cluster_col = FALSE,legend = FALSE,show_rownames=F,show_colnames=F)
 pheatmap(dfP,cluster_row = FALSE, cluster_col = FALSE,show_rownames=F,show_colnames=F)
-write.table(dfP,"D:\\idps\\script\\output\\rrfile\\P30443_dfP.txt",quote = FALSE,row.names = FALSE)
-write.table(df01,"D:\\idps\\script\\output\\rrfile\\P30443_df01.txt",quote = FALSE,row.names = FALSE)
+write.table(dfP,"D:\\idps\\script\\output\\rrfile\\P30443_dfP.txt",quote = FALSE,row.names = FALSE,col.names = F)
+write.table(df01,"D:\\idps\\script\\output\\rrfile\\P30443_df01.txt",quote = FALSE,row.names = FALSE,col.names = F)
+
+#######################################
+###########相邻残基注释为接触##########
+#######################################
+
+df01_2 <- df01
+diag(df01_2[-leng,-1]) <- 1
+diag(df01_2[-1,-leng]) <- 1
+write.table(df01_2,"D:\\idps\\script\\output\\rrfile\\P30443_df01_2.txt",quote = FALSE,row.names = FALSE,col.names = F)
