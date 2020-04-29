a4cb1e3626c26a8e2545f9d02dfa54a9bad67caa
q <sirenfrappe@gmail.com>
2020-04-29 22:04:01 +0800

    4.29
    补网络最短路径和节点数关系图

---
+++ buchong.r
@@ -0,0 +1,15 @@
+library(tidyverse)
+#读取文件
+data <- tibble()
+for (i in dir("D:/idps/igraph_shortest/shortest_path2")) {
+  df <- read_tsv(str_c("D:/idps/igraph_shortest/shortest_path2/",i)) %>%
+    select(-(1:2))
+  len <- dim(df)[1]
+  mean_path <- sum(df)/(len*(len-1))
+  data <- bind_rows(data,tibble(meanPath=mean_path,len=len))
+}
+ggplot(data,aes(x=len,y=meanPath))+
+  geom_point()+
+  geom_smooth(se=FALSE)+
+  xlab("网络节点数")+
+  ylab("平均最短路径")
